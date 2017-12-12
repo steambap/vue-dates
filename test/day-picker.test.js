52fb68229187aa6cdf94a83dc18a95f8bd01ec80
Weilin Shi <934587911@qq.com>
2017-12-12 17:04:27 +0800

    Add circle ci test configuration file

--- test/day-picker.test.js
+++ test/day-picker.test.js
@@ -1,5 +1,6 @@
 import { mount, shallow } from "vue-test-utils";
 import DayPicker from "../src/components/day-picker.vue";
+import { VERTICAL_ORIENTATION } from "../src/constants";
 
 describe("Component", () => {
   test("is a Vue instance", () => {
@@ -8,12 +9,31 @@ describe("Component", () => {
   });
 });
 
-describe("Render week header", () => {
-  test("There are 7 days", () => {
+describe("Render", () => {
+  test("There are 7 days in a week", () => {
     const wrapper = shallow(DayPicker);
     const weekHeaders = wrapper.findAll(".DayPicker__week-header");
     weekHeaders.wrappers.forEach(weekHead => {
-      expect(weekHead.find(li)).toHaveLength(7);
-    })
-  })
+      expect(weekHead.findAll("li")).toHaveLength(7);
+    });
+  });
+
+  test("Number of months props", () => {
+    const numberOfMonths = 3;
+    const wrapper = mount(DayPicker, { propsData: { numberOfMonths } });
+    expect(wrapper.findAll("ul")).toHaveLength(numberOfMonths);
+  });
+
+  test("Vertical orientation props", () => {
+    const orientation = VERTICAL_ORIENTATION;
+    const wrapper = mount(DayPicker, { propsData: { orientation } });
+    expect(wrapper.findAll("ul")).toHaveLength(1);
+  });
+
+  test("Info panel", () => {
+    const className = "test-info-panel";
+    const infoPanel = `<div class=${className}></div>`;
+    const wrapper = mount(DayPicker, { slots: { "info-panel": infoPanel } });
+    expect(wrapper.findAll("." + className)).toHaveLength(1);
+  });
 });
